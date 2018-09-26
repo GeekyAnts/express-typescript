7bb071c32a8cc09b54250c46fb11a5ef861f8654
Faiz A Farooqui <faizahmed@Faizs-MacBook-Air.local>
2018-09-26 12:21:22 +0530

    fixes: event for cluster response after fork

--- dist/exception/NativeEvent.js
+++ dist/exception/NativeEvent.js
@@ -9,9 +9,11 @@ const Log_1 = require("../middlewares/Log");
 class NativeEvent {
     cluster(_cluster) {
         // Catch cluster listening event...
-        _cluster.on('listening', (worker) => Log_1.default.info(`Server :: Cluster with ProcessID '${worker.process.pid}' Connected`));
+        _cluster.on('listening', (worker) => Log_1.default.info(`Server :: Cluster with ProcessID '${worker.process.pid}' Connected!`));
+        // Catch cluster once it is back online event...
+        _cluster.on('online', (worker) => Log_1.default.info(`Server :: Cluster with ProcessID '${worker.process.pid}' has responded after it was forked! `));
         // Catch cluster disconnect event...
-        _cluster.on('disconnect', (worker) => Log_1.default.info(`Server :: Cluster with ProcessID '${worker.process.pid}' Disconnected`));
+        _cluster.on('disconnect', (worker) => Log_1.default.info(`Server :: Cluster with ProcessID '${worker.process.pid}' Disconnected!`));
         // Catch cluster exit event...
         _cluster.on('exit', (worker, code, signal) => {
             Log_1.default.info(`Server :: Cluster with ProcessID '${worker.process.pid}' is Dead with Code '${code}, and signal: '${signal}'`);
--- src/exception/NativeEvent.ts
+++ src/exception/NativeEvent.ts
@@ -9,10 +9,13 @@ import Log from '../middlewares/Log';
 class NativeEvent {
 	public cluster (_cluster): void {
 		// Catch cluster listening event...
-		_cluster.on('listening', (worker) => Log.info(`Server :: Cluster with ProcessID '${worker.process.pid}' Connected`));
+		_cluster.on('listening', (worker) => Log.info(`Server :: Cluster with ProcessID '${worker.process.pid}' Connected!`));
+
+		// Catch cluster once it is back online event...
+		_cluster.on('online', (worker) => Log.info(`Server :: Cluster with ProcessID '${worker.process.pid}' has responded after it was forked! `));
 
 		// Catch cluster disconnect event...
-		_cluster.on('disconnect', (worker) => Log.info(`Server :: Cluster with ProcessID '${worker.process.pid}' Disconnected`));
+		_cluster.on('disconnect', (worker) => Log.info(`Server :: Cluster with ProcessID '${worker.process.pid}' Disconnected!`));
 
 		// Catch cluster exit event...
 		_cluster.on('exit', (worker, code, signal) => {
