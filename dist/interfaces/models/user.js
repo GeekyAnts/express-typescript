4b1e4a5d121a0247f914aca42c0ddcdb275de391
Ahmed Meftah <contact@ahmedmeftah.com>
2022-06-24 20:31:39 +0100

    Remove cached dist files from git

--- dist/interfaces/models/user.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/**
- * Define interface for User Model
- *
- * @author Faiz A. Farooqui <faiz@geekyants.com>
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=user.js.map
